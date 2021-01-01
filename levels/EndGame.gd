8500fb78127dd90ce79568ddf6517f891939e2af
Pawel-9215 <hordyniak@gmail.com>
2021-01-01 19:31:06 +0100

    game is finished

--- levels/EndGame.gd
+++ levels/EndGame.gd
@@ -1,9 +1,12 @@
 extends Control
-
+export var timed = true
 
 func _ready():
 	pass
 
-
 func _on_restart_button_pressed():
 	get_tree().change_scene("res://levels/level1.tscn")
+
+func _on_Timer_timeout():
+	if timed:
+		get_tree().change_scene("res://levels/level1.tscn")
--- levels/GameState.gd
+++ levels/GameState.gd
@@ -2,12 +2,18 @@ extends Node2D
 
 var lives = 3
 var coins = 0
+var player_hurting = false
+var alpha = 1
+
 const WORLD_LIMIT = 369
 const PRICE_OF_LIFE = 10
 
 func _process(delta):
 	if $Player.position.y > WORLD_LIMIT:
 		end_game()
+	if player_hurting:
+		blink()
+		
 
 func _ready():
 	add_to_group("GameState")
@@ -22,15 +28,31 @@ func coin_up():
 	update_gui()
 
 func hurt():
-	lives -= 1
-	$Player.hurt()
-	if lives < 0:
-		end_game()
-	update_gui()
+	if player_hurting == false:
+		player_hurting = true
+		$Timer.start()
+		lives -= 1
+		$Player.hurt()
+		if lives < 0:
+			end_game()
+		update_gui()
 	
 func end_game():
 	get_tree().change_scene("res://levels/EndGame.tscn")
 	
+func win_game():
+	get_tree().change_scene("res://levels/Victory.tscn")
+	
 func update_gui():
 	get_tree().call_group("GUI", "update", lives, coins)
 	
+func blink():
+	if alpha == 0:
+		alpha = 1
+	else:
+		alpha = 0
+	$Player/PlayerAnimation.self_modulate = Color( 0.95, 0, 0, alpha )
+
+func _on_Timer_timeout():
+	player_hurting = false
+	$Player/PlayerAnimation.self_modulate = Color( 1, 1, 1, 1 )
